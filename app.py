75f4200bcf84756209b9a0766ecc2985efbdd8a4
angel3122004 <angelmary23cs@cce.edu.in>
2026-09-17 10:32:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ st.title("Student Pass/Fail based on Study Hours")
 hours=st.number_input("Enter Study Hours: ",min_value=0.0, max_value= 15.0, value=5.0)
 if st.button("Predict"):
   prediction=model.predict([[hours]])
+  pass_probability = model.predict_proba([[hours]])[0][1] * 100
   if prediction[0]==1:
     st.success("Pass")
   else:
