a351f659baf884be50c4afbb2fac3ac4d00d9d5f
angel3122004 <angelmary23cs@cce.edu.in>
2026-09-17 11:38:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import joblib
 model=joblib.load("logistic_regression_studyHrs_model.pkl")
 st.title("Student Pass/Fail based on Study Hours")
-hours=st.number_input("Enter Study Hours: ",min_value=0.0, max_value= 15.0, value=5.0)
-attendance=st.number_input("Enter Attendance: ",min_value=0.0, max_value=100.0, value=75.0)
+hours=st.number_input("Enter Study Hours: ",min_value=4.0, max_value= 15.0, value=5.0)
+attendance=st.number_input("Enter Attendance: ",min_value=60.0, max_value=100.0, value=75.0)
 
 if st.button("Predict"):
   prediction=model.predict([[hours,attendance]])
