c9c6ac063b770d5d1902a9b65d58de27d19368c0
angel3122004 <angelmary23cs@cce.edu.in>
2026-09-18 12:24:30 +0530

    Update display_passfail_gradio.py

--- display_passfail_gradio.py
+++ display_passfail_gradio.py
@@ -4,13 +4,14 @@ import pandas as pd
 import os
 
 # Load model
-model = joblib.load("Student_Std_Hrs.pkl")
+model = joblib.load("logistic_regression_studyHrs_model.pkl")
 
 
-def predict_result(study_hours):
+def predict_result(study_hours, attendance):
 
     input_data = pd.DataFrame({
-        "Study_Hours": [study_hours]
+        "Study_Hours": [study_hours],
+        "Attendance": [attendance]
     })
 
     prediction = model.predict(input_data)[0]
@@ -28,15 +29,28 @@ def predict_result(study_hours):
 
 demo = gr.Interface(
     fn=predict_result,
-    inputs=gr.Number(
-        label="Enter Study Hours",
-        minimum=0,
-        maximum=24,
-        value=5
-    ),
+
+    inputs=[
+        gr.Number(
+            label="Enter Study Hours",
+            minimum=0,
+            maximum=24,
+            value=5
+        ),
+
+        gr.Number(
+            label="Enter Attendance (%)",
+            minimum=0,
+            maximum=100,
+            value=75
+        )
+    ],
+
     outputs=gr.Textbox(label="Prediction"),
+
     title="Student Result Prediction",
-    description="Predict Pass or Fail based on Study Hours."
+
+    description="Predict Pass or Fail based on Study Hours and Attendance."
 )
 
 
